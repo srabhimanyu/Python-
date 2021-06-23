aa389553bfe5305e830f176f7b9cfb492926c95b
srabhimanyu <srabhimanyu@gmail.com>
2021-06-23 15:53:18 +0530

    strings example in python

---
+++ strings.py
@@ -0,0 +1,9 @@
+print("Today is good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in string');
+print("hello"+" world")
+greeting = "Hello"
+name = input("Please enter your name")
+print(greeting + name)
+print(greeting + ' '+ name)
