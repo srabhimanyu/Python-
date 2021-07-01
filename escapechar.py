90f9de13ffa92063325369cabb39a42b611e4b2e
srabhimanyu <srabhimanyu@gmail.com>
2021-07-01 18:17:33 +0530

    Escape sequence example

--- escapechar.py
+++ escapechar.py
@@ -3,3 +3,15 @@ print(splitString)
 
 tabbedString ="1\t2\t3\t4\t5"
 print(tabbedString)
+
+print('The pet shop owner said "No,no, \'e\'s uh....,he\'s resting".')
+# or
+print("The pet shop owner said \"No,no,'e's uh,....he's resting\".")
+print(""""The pet shop owner said "No,no,'e's  uh,....he's resting".""")
+
+anotherSplitString = """The string has been\
+split over\
+several \
+lines """
+
+print(anotherSplitString)
